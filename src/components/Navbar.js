575db73d20db801ec9b0317898f65838124a7bb1
Samuel Airehrour <odianoseairehrour@gmail.com>
2021-04-22 13:49:25 +0100

    fix: fix the dashboard lable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className="d-flex justify-content-between align-items-center navbar">
       <div className="d__color">
-        <h4>Dashboard</h4>
+        <h4 className="pl-3">Dashboard</h4>
       </div>
       <div className="icons mr-5">
         <img src={bell} alt="bell" className="img-fluid mr-4" width="30px" />
